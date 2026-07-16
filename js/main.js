2fbc4d0a8c0b0c96fd0cef094d061ac847050464
Ali mahjoob <behnamuiux@gmail.com>
2026-07-16 12:14:41 +0330

    add hamburger menu

--- js/main.js
+++ js/main.js
@@ -224,3 +224,12 @@ if (loader) {
     }, 1200);
   });
 }
+const hamburger = document.querySelector(".hamburger");
+
+const menu = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+
+  menu.classList.toggle("active");
+});
